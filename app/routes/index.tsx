f42dfcd459baf90c510ace2407ee8646cb3b19d2
Alex Smaliy <alex.smaliy@gmail.com>
2022-09-11 01:44:40 -0700

    maybe? #12

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,12 +11,13 @@ export const loader: LoaderFunction = async function({request, context, params})
   // console.log(JSON.stringify(values))
   // return JSON.stringify(values)
   // @ts-ignore
-  // const val = await context.KV1.get("k");
+  const val = await context.KV1.get("k");
   return JSON.stringify([
-    Object.getPrototypeOf(context.D1.binding),
-    Object.getOwnPropertyNames(context.D1.binding),
-    Object.getPrototypeOf(context.KV1.binding),
-    Object.getOwnPropertyNames(context.KV1.binding),
+    val,
+    Object.getPrototypeOf(context.D1),
+    Object.getOwnPropertyNames(context.D1),
+    Object.getPrototypeOf(context.KV1),
+    Object.getOwnPropertyNames(context.KV1),
   ])
 }
 
