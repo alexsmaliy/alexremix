4c43bff6fbca6f0c814f1960e253170093387135
Alex Smaliy <alex.smaliy@gmail.com>
2022-09-11 09:59:34 -0700

    maybe? #15

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,12 +6,12 @@ export const loader: LoaderFunction = async function({request, context, params})
   // const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json`
   // const response = await fetch(url).then(r => r.json()).then(JSON.stringify)
   // return response
-  // const stmt = await context.D1.binding.exec("select * from bookmarks")
-  // const val = await stmt.first();
+  const stmt = await context.D1.binding.exec("select * from bookmarks")
+  const val = await stmt.first();
   // console.log(JSON.stringify(values))
   // return JSON.stringify(values)
   // @ts-ignore
-  const val = await context.KV1.get("k");
+  // const val = await context.KV1.get("k");
   return JSON.stringify([
     val,
     Object.getPrototypeOf(context.D1),
